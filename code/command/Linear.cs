63068fc6b4743f166bf3187e1aff480d14e36b7e
Hara Red <rtc6fg4.fejg2@gmail.com>
2022-01-15 16:13:20 +0300

    Document the rest of commands

--- code/command/Linear.cs
+++ code/command/Linear.cs
@@ -1,54 +1,98 @@
+using System;
 using Godot;
 
+/// <summary>Perform linear motion to the target poisition.</summary>
 public class Linear : Command
 {
+    /// <summary>Internal reference to the controlled mechanism.</summary>
     private Controllable controllable;
+
+    /// <summary>Internal reference to the execution context.</summary>
     private Context context;
 
+    /// <summary>The start position of the robot.</summary>
     private Target4 start;
+
+    /// <summary>The target pose of the robot.</summary>
     private Pose4 target;
 
+    /// <summary>Maximum linear velocity during the motion execution, millimeter
+    /// per second.</summary>
     private float linearVelocity;
+
+    /// <summary>Maximum angular velocity during the motion execution, degree
+    /// per second.</summary>
     private float angularVelocity;
 
+    /// <summary>Internal progress counter.</summary>
     private float progress;
 
-    public Linear(Pose4 target, float vLin, float vAng)
+    /// <summary>Create new <c>Linear</c> command.</summary>
+    /// <param name="target">Target robot pose.</summary>
+    /// <param name="linearVelocity">Maximum linear velocity during motion
+    /// execution, millimeter per second.</summary>
+    /// <param name="angularVelocity">Maximum angular velocity during motion
+    /// execution, degree per second.</summary>
+    public Linear(Pose4 target, float linearVelocity, float angularVelocity)
     {
+        // Note that the target is Pose4, not Target4. That is due to the robot
+        // performing the shortest motion between the start and the target,
+        // ignoring target flange rotations amount.
         this.target = target;
-        linearVelocity = vLin;
-        angularVelocity = vAng;
+        // Throw an exception if the velocity parameter is wrong.
+        if (linearVelocity <= 0 || angularVelocity <= 0)
+        {
+            throw new ArgumentException("Wrong velocity");
+        }
+        this.linearVelocity = linearVelocity;
+        this.angularVelocity = angularVelocity;
     }
 
     public void Init(Controllable controllable, Context context)
     {
         this.controllable = controllable;
         this.context = context;
+        // Calculate the tool's position in relation to the robot origin. Just
+        // combine the flange position and the TCP position. 
         start = controllable.GetCurrentPosition() * context.Tool;
     }
 
     public State Process(float delta)
     {
+        // Calculate delta pose between the start and the end. To do so solve
+        // simple matrix-like equation:
+        //  S * D = T, find D:
+        //  S^-1 * S * D = S^-1 * T
+        //  D = S^-1 * T
+        // The ^-1 operator analog for Pose4 is Inverse operation.
         Pose4 deltaPose = start.pose.Inverse() * target;
-        float length = linearVelocity / deltaPose.position.Length();
-        float angle = angularVelocity / Mathf.Abs(
-            Mathf.Wrap(deltaPose.rotation, -180, 180)
-        );
 
-        progress += delta * Mathf.Min(length, angle);
+        // Calculate times required to perform the linear motion and the angular
+        // motion individually.
+        float lengthTime = deltaPose.position.Length() / linearVelocity;
+        float angleTime = Mathf.Abs(
+            Mathf.Wrap(deltaPose.rotation, -180, 180)
+        ) / angularVelocity;
+
+        // Update progress taking in mind that both parts of motion should
+        // finish simultaneously;
+        progress += delta / Mathf.Max(lengthTime, angleTime);
         if (progress >= 1)
         {
-            Pose4 tmp = deltaPose * context.Tool.Inverse();
-            Target4 result = start * tmp;
+            // Calculate Target4 by using original target and deltaPose, apply
+            // inverse tool transform to obtain flange position.
+            Target4 result = start * deltaPose * context.Tool.Inverse();
             if (controllable.SetPosition(result) is null)
             {
                 return State.Error;
             }
             return State.Done;
         }
+        // Find intermidiate position.
         Pose4 interpolated = new Pose4().Interpolate(deltaPose, progress);
-        Pose4 withTool = interpolated * context.Tool.Inverse();
-        Target4 resultTarget = start * withTool;
+        // Calculate resulting flange position, apply inverse tool translation
+        // to obtain flange position in relation to the robot origin.
+        Target4 resultTarget = start * interpolated * context.Tool.Inverse();
 
         if (controllable.SetPosition(resultTarget) is null)
         {
--- code/command/PositionHold.cs
+++ code/command/PositionHold.cs
@@ -1,19 +1,29 @@
+/// <summary>Hold position indefinitely. This command is intended to be used
+/// with other commands, like the <c>InputWait</c> command.</summary>
 public class PositionHold : Command
 {
+    /// <summary>Internal reference to the controlled mechanism.</summary>
     private Controllable controllable;
+
+    /// <summary>Internal reference to the execution context.</summary>
     private Context context;
 
+    /// <summary>The target position to hold the mechanism in.</summary>
     private Target4 target;
 
     public void Init(Controllable controllable, Context context)
     {
         this.controllable = controllable;
         this.context = context;
+        // Calculate current tool position using the flange position and TCP
+        // geometry information.
         target = controllable.GetCurrentPosition() * context.Tool;
     }
     public State Process(float delta)
     {
+        // Calculate current flange position.
         Target4 flangeTarget = target * context.Tool.Inverse();
+        // Apply it to the robot.
         if (controllable.SetPosition(flangeTarget) is null)
         {
             return State.Error;
