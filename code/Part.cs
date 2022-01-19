e1d53c1cb628f3043140efaed36e22667a986b64
Hara Red <rtc6fg4.fejg2@gmail.com>
2022-01-19 15:22:38 +0300

    Document Part and Tool

--- code/Part.cs
+++ code/Part.cs
@@ -1,4 +1,6 @@
+/// <summary>The <c>Part</c> is represented by its origin position.</summary>
 interface Part
 {
+    /// <summary>Get this part's origin position.</summary>
     Pose4 GetOrigin();
 }
--- code/Tool.cs
+++ code/Tool.cs
@@ -1,4 +1,6 @@
+/// <summary>The <c>Tool</c> is represented by its TCP position.</summary>
 interface Tool
 {
+    /// <summary>Get this tool's TCP.</summary>
     Pose4 GetToolCenterPoint();
 }
