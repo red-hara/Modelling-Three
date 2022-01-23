4067c00a941de8b02385c7c985e5148128009d6a
Hara Red <rtc6fg4.fejg2@gmail.com>
2022-01-23 22:11:27 +0300

    Document Jointable, Positionable and Controllable

--- code/Jointable.cs
+++ code/Jointable.cs
@@ -1,10 +1,27 @@
+/// <summary>The <c>Jointable</c> instance provides several methods for working
+/// with its joints.</summary>
 public interface Jointable
 {
+    /// <summary>Try setting generalized coordinates for this
+    /// <c>Jointable</c>.</summary>
+    /// <param name="generalized">Generalized coordinates to try to set.</param>
+    /// <returns>Position of this <c>Jointable</c>'s flange if operation
+    /// successful.</returns>
     Target4? SetJoints(Generalized4 generalized);
 
+    /// <summary>Try solving the forward kinematics problem for this
+    /// <c>Jointable</c>.</summary>
+    /// <param name="generalized">Generalized coordinates to solve the forward
+    /// kinematics problem for.</param>
+    /// <returns>Position of this <c>Jointable</c>'s flange for provided
+    /// generalized coordinates if the solution exists.</returns>
     Target4? Forward(Generalized4 generalized);
 
+    /// <summary>Get current generalized coordinates.</summary>
+    /// <returns>Current generalized coordinates.</returns>
     Generalized4 GetCurrentJoints();
 
+    /// <summary>Get maximum joint velocity set.</summary>
+    /// <returns>Maximum joint velocity.</returns>
     Generalized4 MaximumJointVelocity();
 }
--- code/Positionable.cs
+++ code/Positionable.cs
@@ -1,6 +1,21 @@
+/// <summary>The <c>Positionable</c> instance provides several methods for
+/// working with its spatial position.</summary>
 public interface Positionable
 {
+    /// <summary>Try setting spatial position for this
+    /// <c>Positionable</c>.</summary>
+    /// <param name="position">Desired spatial position.</param>
+    /// <returns>Resulting generalized position if the provided position is
+    /// reachable.</returns>
     Generalized4? SetPosition(Target4 position);
+
+    /// <summary>Try solving the inverse kinematics problem for given
+    /// position.</summary>
+    /// <param name="position">Target position.</param>
+    /// <returns>Generalized coordinates if the provided position is
+    /// reachable.</returns>
     Generalized4? Inverse(Target4 position);
+
+    /// <summary>Get current <c>Positionable</c> position.</summary>
     Target4 GetCurrentPosition();
 }
