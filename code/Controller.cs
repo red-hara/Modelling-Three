b16caccc35413414d22328e576f2f98d92997d32
Hara Red <rtc6fg4.fejg2@gmail.com>
2022-01-25 14:07:06 +0300

    Document Controller

--- code/Controller.cs
+++ code/Controller.cs
@@ -1,20 +1,32 @@
 using Godot;
 
+/// <summary>The <c>Controller</c> node interacts with the <c>Context</c> node
+/// and controlls the <c>Controllable</c> node.</summary>
 public class Controller : Node
 {
+
+    /// <summary>Path to the <c>Context</c> node to work with.</summary>
     [Export]
     public NodePath context;
+
+    /// <summary>The <c>Context</c> to work with.</summary>
     private Context ctx;
+
+    /// <summary>Current command to be polled during execution.</summary>
     private Command currentCommand;
 
+    /// <summary>Path to the <c>Controllable</c> node.</summary>
     [Export]
     public NodePath controllable;
 
+    /// <summary>Path to the <c>Trace</c> to be drawn on the current
+    /// TCP.</summary>
     [Export]
     public NodePath tcpTrace;
 
     public override void _Ready()
     {
+        // The context's path has to be initialized before running the program.
         ctx = GetNode<Context>(context);
         ctx.GeneratePath();
     }
@@ -22,6 +34,8 @@ public class Controller : Node
     public override void _Process(float delta)
     {
         Controllable control = GetNode<Controllable>(controllable);
+        // Poll current command if present, else try dequeueing another one from
+        // the Context's queue.
         if (!(currentCommand is null))
         {
             State state = currentCommand.Process(delta);
@@ -47,6 +61,7 @@ public class Controller : Node
 
         Trace trace = GetNode<Trace>(tcpTrace);
         Spatial controlSpatial = (Spatial)control;
+        // Generate transform chain "Controllable -> Flange -> TCP"
         trace.GlobalTransform = controlSpatial.GlobalTransform *
             new Transform(
                 Quat.Identity,
