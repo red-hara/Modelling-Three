68d8285c9aa8c7acd02659cbb2d1d7157b0c2d8a
Hara Red <rtc6fg4.fejg2@gmail.com>
2022-01-28 13:54:44 +0300

    Document SpatialPart

--- prefab/Parts/SpatialPart.cs
+++ prefab/Parts/SpatialPart.cs
@@ -1,14 +1,21 @@
 using Godot;
 
+/// <summary>The <c>SpatialPart</c> is template for custom
+/// <c>Part</c>.</summary>
 class SpatialPart : Spatial, Part
 {
+    public override void _Ready()
+    {
+    }
+
     public override void _Process(float delta)
     {
-
     }
 
     public Pose4 GetOrigin()
     {
+        // Just return position in local space. Ignore BC angles and hope that
+        // they are zero.
         return new Pose4(Translation, RotationDegrees.z);
     }
 }
